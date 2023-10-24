af3b88c0c4548404533a5b771d4fa58dc10c2112
Alexander Kolotov <alexandr.kolotov@gmail.com>
2023-10-24 16:04:35 -0600

    workaround for parsing BigInts in JSON

--- step_2_sign_typed_data.sh
+++ step_2_sign_typed_data.sh
@@ -4,6 +4,11 @@ set -e
 
 source .env
 
+WORKING_TYPED_DATA_FILE=`dirname ${TYPED_DATA_FILE}`/working_`basename ${TYPED_DATA_FILE}`
+sed 's#"value": \(.*\)#"value": "\1"#' ${TYPED_DATA_FILE} > ${WORKING_TYPED_DATA_FILE}
+
 cast wallet sign \
     ${PRIV_KEY_SOURCE} \
-    --data --from-file ${TYPED_DATA_FILE}
+    --data --from-file ${WORKING_TYPED_DATA_FILE}
+
+rm ${WORKING_TYPED_DATA_FILE}
